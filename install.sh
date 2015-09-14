315a0bcb607821121d3d68f029025887809dbcb2
G4RUF <guillaumefavre@live.fr>
2015-09-14 11:17:09 +0200

    configure bash by default in the installation

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # Created By        : Guillaume FAVRE
 # Creation Date     : juin 23th, 2015
 # Version           : 0.1
-# Last Change       : September 10th, 2015 at 11:38:39
+# Last Change       : September 14th, 2015 at 10:48:21
 # Last Changed By   : Guillaume FAVRE
 # Purpose           : Description
 #
@@ -13,20 +13,25 @@
 
 #Function to download different files
 if [ $(id -u) = 0 ]; then
-	listPrograms='xorg gnome3 vim firefox libreoffice hping'
+	listPrograms='bash xorg gnome3 vim firefox libreoffice hping'
 	for program in $listPrograms; do
 		echo "Install " $program   
 		pkg install --yes $program
-		if [ program = 'gnome3' ]; then
-			gdm=$(cat /etc/rc.conf | grep 'gdm')
-			gnome=$(cat /etc/rc.conf | grep 'gnome')
-		       	if [ "$gdm" != 'gdm_enable="YES"']; then
-				echo 'gdm_enable="YES"' >> /etc/rc.conf
-			fi
-		       	if [ "$gnome" != 'gnome_enable="YES"']; then
-				echo 'gnome_enable="YES"' >> /etc/rc.conf
-			fi
+		case $program in
+		'gnome3')
+		gdm=$(cat /etc/rc.conf | grep 'gdm')
+		gnome=$(cat /etc/rc.conf | grep 'gnome')
+		if [ "$gdm" != 'gdm_enable="YES"']; then
+			echo 'gdm_enable="YES"' >> /etc/rc.conf
 		fi
+		if [ "$gnome" != 'gnome_enable="YES"']; then
+			echo 'gnome_enable="YES"' >> /etc/rc.conf
+		fi
+		;;
+		'bash')
+		chsh -s /usr/local/bin/bash root
+		;;
+		esac
 	done
 	bash init.sh
 else
